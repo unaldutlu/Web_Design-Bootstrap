c642a9c71e5e674443e8be797a048f842de7f048
unaldutlu <unaldutlu@gmail.com>
2022-09-23 23:31:42 +0300

    portfolio modal added

--- js/main.js
+++ js/main.js
@@ -20,3 +20,9 @@ window.addEventListener("DOMContentLoaded", event => {
         });
     }
 });
+var BtnCanvas = document.querySelectorAll(".btn-close-canvas");
+for (let i = 0; i < BtnCanvas.length; i++) {
+    BtnCanvas[i].addEventListener("click", function () {
+        document.querySelector('[data-bs-dismiss="offcanvas"]').click();
+    });
+}
